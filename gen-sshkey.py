5efe8b6d2ce9a9fea587adbd341e86427c84a4bb
Tennex <nneal@Tennexs-MBP.home>
2020-11-20 15:00:13 -0500

    added more to gen-sshkey.py

--- gen-sshkey.py
+++ gen-sshkey.py
@@ -1,11 +1,16 @@
 import os
 import subprocess
 import getpass
+import requests
+import json
+import time
 from Crypto.PublicKey import RSA
 
 # install pycrypto
 GROUP_MEMBERSHIP_LOOKUP = "LOCAL" # can be LDAP or LOCAL
 GROUP_MEMBERSHIP_LOOKUP_IDENTIFIER = "ssh-" # used to locate different groups.
+VAULT_SERVER = "vault.local:8200"
+VAULT_HTTPS = False
 
 # user = os.environ.get('USER') # vulnerable
 user = subprocess.check_output(["id", "-u", "-n"]).decode("utf-8").strip("\n")
@@ -57,6 +62,14 @@ while True:
 
 subprocess.Popen(["ssh-keygen", "-t", "rsa", "-b", "2048", "-f", f"{private_key}", "-q", "-N", f"{key_passphrase}"])
 
+# adding to give time to write public key
+time.sleep(1)
+
+public_key_contents = ""
+f=open(public_key, "r")
+if f.mode == "r":
+    public_key_contents = f.read().strip("\n")
+
 # had to use a different method
 #key = RSA.generate(2048)
 #with open(private_key, 'wb') as content_file:
@@ -68,15 +81,54 @@ subprocess.Popen(["ssh-keygen", "-t", "rsa", "-b", "2048", "-f", f"{private_key}
 #    content_file.write(pubkey.exportKey('OpenSSH'))
 
 # read roles from LDAP or local group membership
-groups = []
+groups = ""
 if GROUP_MEMBERSHIP_LOOKUP == "LOCAL":
-     tmp_groups = subprocess.check_output(["groups"]).decode("utf-8").strip("\n").split()
-     for g in tmp_groups:
-         if GROUP_MEMBERSHIP_LOOKUP_IDENTIFIER in g:
-             groups.append(g)
+    tmp_groups = subprocess.check_output(["groups"]).decode("utf-8").strip("\n").split()
+    for g in tmp_groups:
+        if GROUP_MEMBERSHIP_LOOKUP_IDENTIFIER in g:
+            groups = groups + g + " "
 
-print(groups)
+    groups = groups.strip()
+
+
 #elif GROUP_MEMBERSHIP_LOOKUP == "LDAP":
 
+# call vault API to get client token
+
+vault_user = getpass.getuser("Enter vault username: ")
+vault_pass = getpass.getpass("Enter vault password: ")
+
+login_endpoint = f"v1/auth/userpass/login/{vault_user}"
+vault_login_url = ""
+if VAULT_HTTPS:
+    vault_login_url = f"https://{VAULT_SERVER}/{login_endpoint}"
+else:
+    vault_login_url = f"http://{VAULT_SERVER}/{login_endpoint}"
+
+login_data = {"password": vault_pass}
+data = json.dumps(login_data)
+
+r = requests.post(url=vault_login_url, data=data)
+response = json.loads(r.text)
+client_token = response["auth"]["client_token"]
 
 # call vault API to sign public key
+
+key_name = f"{user}-key"
+ca_endpoint = f"v1/ssh/sign/{key_name}"
+vault_ca_url = ""
+if VAULT_HTTPS:
+    vault_ca_url = f"https://{VAULT_SERVER}/{ca_endpoint}"
+else:
+    vault_ca_url = f"http://{VAULT_SERVER}/{ca_endpoint}"
+
+headers = {"X-Vault-Token" : "<TOKEN>"}
+ca_data = {
+    "public_key": public_key_contents,
+    "ttl": "12h",
+    "valid_principals": groups,
+    "cert_type": "user",
+    "key_id": user
+    }
+
+data = json.dumps(ca_data)
